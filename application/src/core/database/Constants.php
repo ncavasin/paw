0fe1f5f750ca8c2a8baee3b9207ef10725546f99
ncavasin <ncavasin97@gmail.com>
2021-05-28 08:59:19 -0300

    modif constants.php

--- application/src/core/database/Constants.php
+++ application/src/core/database/Constants.php
@@ -2,9 +2,9 @@
 
 namespace Paw\core\database;
 
-const _USER_NOM_AP_MAX = 50;
-const _ESPECIALIDAD_NOM_MAX = 50;
-const _ESPECIALIDAD_DESC_MAX = 250;
-#const _SERVICIOS_MAX = 250;
+const USER_NOM_AP_MAX = 50;
+const ESPECIALIDAD_NOM_MAX = 50;
+const ESPECIALIDAD_DESC_MAX = 250;
+#const SERVICIOS_MAX = 250;
 
 ?>
