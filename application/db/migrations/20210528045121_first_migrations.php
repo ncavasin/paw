782be1c0a364b489ae4aa99ce7e2cd5c0776cce4
ncavasin <ncavasin97@gmail.com>
2021-05-29 12:17:15 -0300

    push parcial de la migracion de creacion tablas

--- application/db/migrations/20210528045121_first_migrations.php
+++ application/db/migrations/20210528045121_first_migrations.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
+
+use Paw\core\database\Constants;
 use Phinx\Migration\AbstractMigration;
+
 
 final class FirstMigrations extends AbstractMigration
 {
@@ -17,18 +20,63 @@ final class FirstMigrations extends AbstractMigration
      */
     public function change(): void
     {
-        # Phinx insters an autoincremental ID as PK for every created table unless modified
-        $tableEspecialidad = $this->table('especialidades');
-        $tableEspecialidad->addColumn('nombre', 'string', ['limit' => constant('_ESP_NOM_MAX'),
+
+        $tableObrasSociales = $this->table('obras_sociales', 
+                                          ['id' => false, 'primary_key' => ['nombre']]);
+        $tableObrasSociales->addColumn('nombre', 'string', ['limit' => Constants::getOsNomMax(),
                                                             'null' => false])
-                          ->addColumn('descripcion', 'string', ['limit' => constant('_ESP_DESC_MAX'),
+                            ->create();
+
+        
+        $tableEspecialidad = $this->table('especialidades',
+                                         ['id' => false, 'primary_key' => ['nombre']]);
+        $tableEspecialidad->addColumn('nombre', 'string', ['limit' => Constants::getEspNomMax(),
+                                                            'null' => false])
+                          ->addColumn('descripcion', 'string', ['limit' => Constants::getEspDescMax(),
                                                                 'null'  => false])
                           ->create();
 
-        $tableEspecialista = $this->table('especialistas');
-        $tableEspecialista->addColumn('nombre', 'string', ['limit' => constant('_NOM_AP_MAX')])
-                          ->addColumn('apellido', 'string', ['limit' => constant('_NOM_AP_MAX'),
-                                            'null'  => false])
+        # PK => nombre + apellido
+        # Restriccion => sin nombres duplicados
+        $tableEspecialista = $this->table('especialistas',
+                                         ['id' => false, 'primary_key' => ['nombre','apellido']]);
+        $tableEspecialista->addColumn('nombre', 'string', ['limit' => Constants::getNomApMax(),
+                                                           'null' => false])
+                          ->addColumn('apellido', 'string', ['limit' => Constants::getNomApMax(),
+                                                             'null'  => false])
                           ->create();
+
+        # PK compuesta => FK(especialidad) + FK(especialista)
+        $tableIntermedia = $this->table('intermedia', 
+                                       ['id' => false, 'primary_key' => ['especialidad', 'especialista']]);
+        $tableIntermedia->addColumn('especialidad', 'string', ['limit' => Constants::getEspNomMax(),
+                                                         'null' => false])
+                        ->addColumn('especialista', 'string', ['limit' => Constants::getNomApMax(),
+                                                         'null' => false])
+                                        # col local   | tbl externa  | col externa
+                        ->addForeignKey('especialidad', 'especialidades', 'nombre')
+                                        # col local   | tbl externa  | col externa
+                        ->addForeignKey('especialista', 'especialistas', 'nombre')
+                        ->create();
+
+        # PK => mail
+        # Restriccion => no puede existir un mail asociado a dos cuentas diferentes
+        $tableUsuario = $this->table('usuarios', ['id' => false, 'primary_key' => ['mail']]);
+        $tableUsuario->addColumn('nombre', 'string', ['limit' => Constants::getNomApMax(),
+                                                            'null' => false])
+                        ->addColumn('apellido', 'string', ['limit' => Constants::getNomApMax(),
+                                                            'null'  => false])
+                        ->addColumn('fnac', 'date', ['null' => false])
+                        ->addColumn('celular', 'string'. ['limit' => Constants::getCelMax(),
+                                                          'null' => false])
+                        ->addColumn('mail', 'string', ['limit' => Constants::getMailMax(),
+                                                       'null' => false)
+                        ->addColumn('password', 'string', ['limit' => Constants::getPwdMax(),
+                                                           'null' => false])
+                        ->addColumn('obra_social', 'string', ['limit' => Constants::getOsNomMax(),
+                                                              'null' => true])
+                                       # col local   | tbl externa  | col externa
+                        ->addForeignKey('obra_social', 'obra_social', 'id')
+                        ->create();
     }
 }
