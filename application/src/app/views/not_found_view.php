492e574326b464512983bcb493f9d0a921812d90
ncavasin <ncavasin97@gmail.com>
2021-06-17 18:00:54 -0300

    fix css del 404

--- application/src/app/views/not_found_view.php
+++ application/src/app/views/not_found_view.php
@@ -2,13 +2,14 @@
 <html lang="es-AR">
     <head>
         <?php require 'parts/head_view.php' ?>
+        <link rel="stylesheet" type='text/css' href="/assets/css/not_found.css"/>
     </head>
     <body>
         <?php 
             require 'parts/header_view.php';
         ?>
         <main> 
-            <h1> Error 404: Página no encontrada </h1>
+            <h1> ERROR 404: Página no encontrada</h1>
         </main>
 
         <?php 
