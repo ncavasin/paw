96028952aa42dd8e7df6e8b6fee5e427d7837d7c
ncavasin <ncavasin97@gmail.com>
2021-06-22 23:16:26 -0300

    se eliminan .html

--- application/src/app/views/parts/header_view.php
+++ application/src/app/views/parts/header_view.php
@@ -1,16 +1,14 @@
 <header>
-    
-    <ul class="menu_user">
-        <li><a href="<?= $this->contact['href']?>" class="telefono"><?= $this->contact['name']?></a></li>
-        <?php foreach($this->userOptions as $uOpt):?>
-            <li><a href="<?= $uOpt['href']?>" target="_self" class="user_opt"><?=$uOpt['name']?></a></li>
-        <?php endforeach; ?>
-    </ul>
+    <h1 class="logotipo">
+        <a href="../" target="_self">Dental<br>Medical<br>Group</a>
+    </h1>
     <nav>
-        <h1 class="logotipo">
-            <a href="../" target="_self">Dental<br>Medical<br>Group</a>
-        </h1>
-        <ul class="main_menu">
+        <ul class="menu_user">
+            <?php foreach($this->userOptions as $uOpt):?>
+                <li><a href="<?= $uOpt['href']?>" target="_self" class="user_opt"><?=$uOpt['name']?></a></li>
+            <?php endforeach; ?>
+        </ul>
+        <ul class="main_menu" id='main_menu'>
             <?php foreach($this->menuOptions as $mOpt):?>
                 <li class="main_menu_opt"><a href="<?= $mOpt['href']?>" target="_self" ><?=$mOpt['name']?></a></li>
             <?php endforeach; ?>
