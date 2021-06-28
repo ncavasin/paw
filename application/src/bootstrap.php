fabc906931221b53e5835e1adec9f47918803aca
ncavasin <ncavasin97@gmail.com>
2021-06-28 20:25:49 -0300

    se integra twig

--- application/src/bootstrap.php
+++ application/src/bootstrap.php
@@ -21,6 +21,11 @@ $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
+# Twig
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/app/views');
+$twig = new \Twig\Environment($loader, [
+    'cache' => __DIR__ . '/app/views/cache_twig/',
+]);
 
 # Load enviroment vars
 $dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../');
